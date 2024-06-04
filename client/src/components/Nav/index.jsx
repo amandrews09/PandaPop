7e66032f46d50450f70cea5a8c00789a67c0d1ad
Josh-Manley <joshmanley45@gmail.com>
2024-06-03 20:01:20 -0600

    fixed nav bar when logged in and category files

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,20 +1,35 @@
 import Auth from '../../utils/auth';
 import { Link } from 'react-router-dom';
-import Cart from "../Cart";
+import Cart from '../Cart';
 import './style.css';
 
 function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
+        <ul className="d-flex flex-column align-items-center">
           <li className="mx-1">
-            <Link to="/orderHistory">Order History</Link>
+            <Link to="/home">HOME</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/browse">BROWSE</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/contact">CONTACT</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/signup">SIGNUP</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/login">LOGIN</Link>
+          </li>
+          <li className="mx-1">
+            <Link to="/orderHistory">ORDER HISTORY</Link>
           </li>
           <li className="mx-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
-              Logout
+              LOGOUT
             </a>
           </li>
         </ul>
@@ -44,6 +59,7 @@ function Nav() {
 
   return (
     <header className="">
+      <Cart />
       <h1>
         <Link to="/">
           <img className="w-75 ms-4 mt-5" src="../src/assets/pandapop-logo.jpg" alt="logo" />
@@ -52,8 +68,7 @@ function Nav() {
 
       <nav>{showNavigation()}</nav>
 
-      <Cart />
-
+      
     </header>
   );
 }
